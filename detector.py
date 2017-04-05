2c7d710de9ca66d3604124c594dc961000b192a6
bound2 <9380102+bound2@users.noreply.github.com>
2017-04-06 02:08:26 +0300

    add face cascade and detector class

---
+++ detector.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import cv2
+
+class Cv2HumanDetector:
+
+    def __init__(self):
+        self.cascade = cv2.CascadeClassifier("/cascades/haarcascade_frontalface_default.xml")
+
+    def is_potentially_human(image_path, max_human_count):
+        raw_image = cv2.imread(image_path)
+        grayscale_image = cv2.cvtColor(raw_image, cv2.COLOR_BGR2GRAY)
+
+        faces = self.cascade.detectMultiScale(gray, scaleFactor = 1.1,
+                                             minNeighbors = 5, minSize=(30, 30),
+                                             flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+        face_count = len(faces)
+        return face_count > 0 and face_count <= max_human_count
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from detector import Cv2HumanDetector
 import wget
 
 DONWLOAD_DIR = 'download'
@@ -9,6 +10,7 @@ DISCARDED_DIR = 'discarded'
 
 if __name__ == "__main__":
     driver = webdriver.Chrome()
+    detector = Cv2HumanDetector()
     try:
         driver.get("https://www.instagram.com/explore/tags/tongue/")
         network_entries = driver.execute_script("return window.performance.getEntries();")
@@ -18,6 +20,8 @@ if __name__ == "__main__":
                 image_url = entry_dict.get('name')
                 file_name = wget.download(image_url, out = DONWLOAD_DIR)
                 image_set.add(file_name)
+
+        #for image in image_set:
 
 
         # Download all and check with openCV if human
