f1107aa35171b85cb47827953c12763f85187360
bound2 <9380102+bound2@users.noreply.github.com>
2017-04-06 01:30:12 +0300

    initial commit

---
+++ insta-selenium.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+if __name__ == "__main__":
+    driver = webdriver.Chrome()
+    try:
+        driver.get("https://www.instagram.com/explore/tags/tongue/")
+        network_entries = driver.execute_script("return window.performance.getEntries();")
+        image_set = set()
+        for entry_dict in network_entries:
+            if entry_dict.get('initiatorType') == 'img':
+                image_set.add(entry_dict.get('name'))
+
+        # Download all and check with openCV if human
+        # Then clear entries from driver and scroll further
+
+        #sleep(250)
+        #driver.execute_script("window.scrollTo(document.body.scrollTop, document.body.scrollHeight)")
+    finally:
+        driver.close()
